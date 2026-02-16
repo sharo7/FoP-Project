96c5f2d45a733800996ac8bf7187010a98247bab
HosseinHajnaseri <hossein1385.hajnaseri@gmail.com>
2026-02-16 09:41:37 +0330

    just making everything a little bit cleaner

--- sound_commands.h
+++ sound_commands.h
@@ -1,12 +1,20 @@
 #ifndef SOUND_COMMANDS_H
 #define SOUND_COMMANDS_H
 #include "sprite.h"
+
 //Mix_OpenAudio(int frequency, Uint16 format, int channels, int chunksize) is for initializing the sound and
 // should be added to the main
-void addSound(Sprite& sprite, const string& soundFile, const string& SoundName);
-void startSound(Sprite& sprite, const string& soundName);
-void playSoundUntilDone(Sprite& sprite, const string& soundName);//the program will stop until the sound is played
-void stopAllSounds(Sprite& sprite);
-void setVolumeTo(Sprite& sprite, const string& soundName, int volume);
-void changeVolumeBy(Sprite& sprite, const string& soundName, int volume);
+
+void addSound(Sprite &sprite, const string &soundFile, const string &SoundName);
+
+void startSound(Sprite &sprite, const string &soundName);
+
+void playSoundUntilDone(Sprite &sprite, const string &soundName); //the program will stop until the sound is played
+
+void stopAllSounds(Sprite &sprite);
+
+void setVolumeTo(Sprite &sprite, const string &soundName, int volume);
+
+void changeVolumeBy(Sprite &sprite, const string &soundName, int volume);
+
 #endif //SOUND_COMMANDS_H
--- sprite.h
+++ sprite.h
@@ -3,20 +3,21 @@
 #include <bits/stdc++.h>
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_mixer.h>
+
 using namespace std;
-struct Sprite
-{
-    double xCenter, yCenter;//sprite's center coordinates on the sdl default coordination system
-    double direction;//the angle in degrees
+
+struct Sprite {
+    double xCenter, yCenter; //sprite's center coordinates on the sdl default coordination system
+    double direction; //the angle in degrees
     //direction 0 is pointing right and rotation direction is anticlockwise
     bool visible;
-    SDL_Texture* spriteTexture;
-    int spriteWidth, spriteHeight;//SDL_QueryTexture(spriteTexture, nullptr, nullptr, &spriteWidth, &spriteHeight);
+    SDL_Texture *spriteTexture;
+    int spriteWidth, spriteHeight; //SDL_QueryTexture(spriteTexture, nullptr, nullptr, &spriteWidth, &spriteHeight);
     bool saying;
     bool thinking;
-    map<string, Mix_Chunk*> spriteSounds;
-
+    map<string, Mix_Chunk *> spriteSounds;
 };
-void correctDirRange(Sprite& sprite);//valid range for the angle is -180 to 180 degrees
-Sprite createSprite(SDL_Texture* texture, int windowWidth, int windowLength);
+
+void correctDirRange(Sprite &sprite); //valid range for the angle is -180 to 180 degrees
+Sprite createSprite(SDL_Texture *texture, int windowWidth, int windowLength);
 #endif //SPRITE_H
